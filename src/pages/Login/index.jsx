d9b52c1d81e09088decd94bb28d6dc0122e9eacd
FlightLoong <1577886278@qq.com>
2022-01-18 11:49:00 +0800

    绘制登录表单

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,32 @@
-import { Button } from 'antd'
+import { Form, Input, Button, Checkbox, Card } from 'antd'
+import './index.scss'
+import logo from '@/assets/logo.png'
 
 const Login = () => {
   return (
-    <div>
-      <p>登录页面</p>
-      <Button type="primary">Button</Button>
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
