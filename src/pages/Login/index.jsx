ba2a63b5faa3216bba0315528e39f5571128349d
FlightLoong <1577886278@qq.com>
2022-01-18 15:32:14 +0800

    获取登录表单值

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,19 +3,63 @@ import './index.scss'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
+  // 提交表单
+  const onFinish = (values) => {
+    console.log(values)
+  }
+
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form>
-          <Form.Item>
-            <Input size="large" placeholder="请输入手机号" />
+        <Form
+          onFinish={onFinish}
+          validateTrigger={['onChange', 'onBlur']}
+          initialValues={{
+            mobile: '13911111111',
+            code: '246810',
+            isAgree: true
+          }}
+        >
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: '请输入手机号' },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机格式不正确'
+              }
+            ]}
+          >
+            <Input value="13911111111" size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
-            <Input size="large" placeholder="请输入验证码" />
+          <Form.Item
+            name="code"
+            rules={[
+              { required: true, message: '请输入验证码' },
+              { len: 6, message: '验证码6个字符串' }
+            ]}
+          >
+            <Input value="246810" size="large" placeholder="请输入验证码" />
           </Form.Item>
-          <Form.Item>
+          {/* 
+            此处需要将 valuePropName 属性值设置为 checked，也就是下面设置的内容
+      	    原因：Form.Item 默认操作的是 value 属性，所以，如果 Form.Item 的子节点的值不是 value ，就需要设置为它自己的值
+      			比如，Checkbox 组件操作的是 checked，因此，需要将 valuePropName 设置为 "checked"
+          */}
+          <Form.Item
+            name="isAgree"
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) => {
+                  if (value === true) return Promise.resolve()
+                  else return Promise.reject(new Error('请勾选我已阅读并同意'))
+                }
+              }
+            ]}
+          >
             <Checkbox className="login-checkbox-label">
               我已阅读并同意「用户协议」和「隐私条款」
             </Checkbox>
