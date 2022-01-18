a8e87f3f939b4b74260d73951f5aeef78ce31fa6
FlightLoong <1577886278@qq.com>
2022-01-18 10:33:04 +0800

    集成 Antd 组件库

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,12 @@
+import { Button } from 'antd'
+
 const Login = () => {
-  return <div>登录页面</div>
+  return (
+    <div>
+      <p>登录页面</p>
+      <Button type="primary">Button</Button>
+    </div>
+  )
 }
 
 export default Login
