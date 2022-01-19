09d967d96f0a5c724ef6c55406e569a9ee49588b
FlightLoong <1577886278@qq.com>
2022-01-19 10:39:57 +0800

    Layout布局

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.scss'
 
 // 导入路由组件
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // 导入页面组件
 import Login from '@/pages/Login'
-import Layout from '@/pages/Layout'
+import GeetLayout from '@/pages/Layout'
 import Home from '@/pages/Home'
 
 // 导入权限拦截组件
@@ -14,30 +14,18 @@ import AuthRoute from '@/component/AuthRoute'
 function App() {
   return (
     <BrowserRouter>
-      <div className="app">
-        <h1>Welcome to React Router!</h1>
-
-        <p>
-          <Link to="/login">跳转到登录页面</Link>
-        </p>
-        <p>
-          <Link to="/">跳转到主页</Link>
-        </p>
-
-        <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route path="/login" element={<Login />} />
-
-          <Route
-            path="/home"
-            element={
-              <AuthRoute>
-                <Home />
-              </AuthRoute>
-            }
-          />
-        </Routes>
-      </div>
+      <Routes>
+        {/* <Route path="/" element={<GeetLayout />}></Route> */}
+        <Route
+          path="/home"
+          element={
+            <AuthRoute>
+              <GeetLayout />
+            </AuthRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   )
 }
