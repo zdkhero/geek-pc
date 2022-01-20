57ac9338f292c6fd32b2b375399fe47bd75f3fce
FlightLoong <1577886278@qq.com>
2022-01-20 11:41:01 +0800

    内容管理结构搭建

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
 
+import 'moment/locale/zh-cn'
+import locale from 'antd/lib/locale/zh_CN'
+import { ConfigProvider } from 'antd'
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConfigProvider locale={locale}>
+      <App />
+    </ConfigProvider>
   </Provider>,
   document.querySelector('#root')
 )
--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,5 +1,104 @@
+import { Form, Button, Card, Breadcrumb, Radio, Select, DatePicker, Table, Space } from 'antd'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+
+import styles from './index.module.scss'
+
 const Article = () => {
-  return <div>内容管理</div>
+  const columns = [
+    {
+      title: '封面',
+      dataIndex: 'cover',
+      key: 'cover',
+      render: () => '自定义封面'
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      key: 'title'
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      key: 'status',
+      render: () => '自定义状态'
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'pubdate',
+      key: 'pubdate'
+    },
+    {
+      title: '阅读数',
+      dataIndex: 'read_count',
+      key: 'read_count'
+    },
+    {
+      title: '评论数',
+      dataIndex: 'comment_count',
+      key: 'comment_count'
+    },
+    {
+      title: '点赞数',
+      dataIndex: 'like_count',
+      key: 'like_count'
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: () => (
+        <Space size="middle">
+          <Button type="link" icon={<EditOutlined />} />
+          <Button type="link" icon={<DeleteOutlined />} />
+        </Space>
+      )
+    }
+  ]
+
+  return (
+    <div className={styles.root}>
+      <Card
+        title={
+          // 面包屑
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <Link to="/">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>内容管理</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+      >
+        {/* 表单 */}
+        <Form>
+          <Form.Item label="状态：" name="status">
+            <Radio.Group>
+              <Radio value={undefined}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>已通过</Radio>
+              <Radio value={3}>已拒绝</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="频道：" name="channel_id">
+            <Select style={{ width: 288 }} placeholder="请选择所属频道">
+              <Select.Option value={1}>Java</Select.Option>
+              <Select.Option value={2}>前端</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="日期：" name="dateArr">
+            <DatePicker.RangePicker />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary">筛选</Button>
+          </Form.Item>
+        </Form>
+      </Card>
+
+      <Card title={`根据筛选条件共查询到 100 条结果：`} style={{ marginTop: 24 }}>
+        <Table columns={columns} dataSource={[]}></Table>
+      </Card>
+    </div>
+  )
 }
 
 export default Article
