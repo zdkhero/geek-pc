c7778a9cb51d3085024c0709f8eb38cb998523c0
FlightLoong <1577886278@qq.com>
2022-01-21 18:58:46 +0800

    自定义环境变量

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,18 @@ import thunk from 'redux-thunk'
 // 导入根 reducer
 import rootReducer from './reducers'
 
-import { composeWithDevTools } from 'redux-devtools-extension'
+// import { composeWithDevTools } from 'redux-devtools-extension'
 
-const middlewares = composeWithDevTools(applyMiddleware(thunk))
+let middlewares
+
+if (process.env.NODE_ENV === 'production') {
+  // 生产环境，只启用 thunk 中间件
+  middlewares = applyMiddleware(thunk)
+} else {
+  // 开发环境
+  const { composeWithDevTools } = require('redux-devtools-extension')
+  middlewares = composeWithDevTools(applyMiddleware(thunk))
+}
 
 const store = createStore(rootReducer, middlewares)
 
