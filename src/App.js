3f70814f504b802f929ad2d3738f721b6a0e880e
FlightLoong <1577886278@qq.com>
2022-01-21 18:44:12 +0800

    路由懒加载

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
+import { lazy, Suspense } from 'react'
 import './App.scss'
 
 // 导入路由组件
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-// 导入页面组件
-import Login from '@/pages/Login'
-import GeetLayout from '@/pages/Layout'
 // import Home from '@/pages/Home'
 
 // 导入权限拦截组件
 import AuthRoute from '@/component/AuthRoute'
 
-import Dashboard from '@/pages/Dashboard'
-import Article from '@/pages/Article'
-import Publish from '@/pages/Publish'
-import NotFound from '@/pages/NotFound'
+// 导入页面组件
+const Login = lazy(() => import('@/pages/Login'))
+// import Login from '@/pages/Login'
+const GeetLayout =  lazy(() => import('@/pages/Layout'))
+const Dashboard =  lazy(() => import('@/pages/Dashboard'))
+const Article =  lazy(() => import('@/pages/Article'))
+const Publish =  lazy(() => import('@/pages/Publish'))
+const NotFound =  lazy(() => import('@/pages/NotFound'))
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthRoute><GeetLayout /></AuthRoute>}>
-          {/* index 默认子路由 */}
-          <Route index element={<AuthRoute><Dashboard /></AuthRoute>} />
-          <Route path="article" element={<AuthRoute><Article /></AuthRoute>} />
-          <Route path="publish" element={<AuthRoute><Publish /></AuthRoute>} >
-            <Route path=":id" element={<AuthRoute><Publish /></AuthRoute>} />
+      <Suspense fallback={<div className="loading">loading...</div>}>
+        <Routes>
+          <Route path="/" element={<AuthRoute><GeetLayout /></AuthRoute>}>
+            {/* index 默认子路由 */}
+            <Route index element={<AuthRoute><Dashboard /></AuthRoute>} />
+            <Route path="article" element={<AuthRoute><Article /></AuthRoute>} />
+            <Route path="publish" element={<AuthRoute><Publish /></AuthRoute>} >
+              <Route path=":id" element={<AuthRoute><Publish /></AuthRoute>} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
           </Route>
-          <Route path="*" element={<NotFound />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-      </Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
