a163202b9ee144cd527b327e8fc04978c80bc202
FlightLoong <1577886278@qq.com>
2022-01-21 18:49:38 +0800

    配置 CDN

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { whenProd, getPlugin, pluginByName } = require('@craco/craco')
 
 module.exports = {
   // 修改 webpack 配置
@@ -8,5 +9,30 @@ module.exports = {
       // 约定：使用 @ 表示 src 文件所在路径
       '@': path.resolve(__dirname, 'src')
     }
+  },
+
+  configure: (webpackConfig) => {
+    // 修改webpack配置
+    whenProd(() => {
+      webpackConfig.externals = {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        redux: 'Redux'
+      }
+
+      const { isFound, match } = getPlugin(webpackConfig, pluginByName('HtmlWebpackPlugin'))
+
+      if (isFound) {
+        match.userOptions.cdn = {
+          js: [
+            'https://cdn.bootcdn.net/ajax/libs/react/17.0.2/umd/react.production.min.js',
+            'https://cdn.bootcdn.net/ajax/libs/react-dom/17.0.2/umd/react-dom.production.min.js',
+            'https://cdn.bootcdn.net/ajax/libs/redux/4.1.0/redux.min.js'
+          ]
+        }
+      }
+    })
+
+    return webpackConfig
   }
 }
